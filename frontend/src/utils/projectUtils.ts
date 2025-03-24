e9a781e4116e4406d0ab1b897d9efd0e933b457a
mas <m.yoshida553@gmail.com>
2025-03-25 01:04:36 +0900

    fix: プレースホルダー画像のURLをPicsumに変更

--- frontend/src/utils/projectUtils.ts
+++ frontend/src/utils/projectUtils.ts
@@ -9,11 +9,11 @@ export const formatProjectForCard = (project: Project): ProjectCardProps => {
     targetAmount: project.target_amount,
     currentAmount: project.current_amount || 0,
     deadline: project.deadline,
-    thumbnail_url: project.thumbnail_url || 'https://placehold.co/400x200',
+    thumbnail_url: project.thumbnail_url || 'https://picsum.photos/400/200',
     supporters_count: project.supporters_count,
     creator: project.user ? {
       name: project.user.name,
-      avatarUrl: project.user.profile_image_url || 'https://placehold.co/100x100'
+      avatarUrl: project.user.profile_image_url || 'https://picsum.photos/100/100'
     } : undefined,
     office_approved: project.office_approved
   };
